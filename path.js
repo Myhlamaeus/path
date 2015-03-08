70d106c86462311f524c943d5696cd0b63272c03
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-08 14:35:33 +0000

    Export Path as class

--- path.js
+++ path.js
@@ -1,41 +1,18 @@
 import PercentEncoder from "percent-encoder";
 
 const pEncoder = Symbol("encoder"),
-    decode = PercentEncoder.decode.bind(PercentEncoder),
-    Path = Object.freeze(Object.assign(function(desc) {
-            Object.assign(this, desc);
-        }, {
-            "defaultSeparator": "/",
-            "parse": function(path, separator = Path.defaultSeparator) {
-                const directories = path.split(separator).map(decode);
+    decode = PercentEncoder.decode.bind(PercentEncoder);
 
-                var file;
-                if(!path.endsWith(separator)) {
-                    file = directories.pop();
-                }
+class Path {
+    constructor(desc) {
+        Object.assign(this, desc);
+    }
 
-                const path = new Path({
-                    "directories": directories,
-                    "file": file,
-                    "separator": separator
-                });
-                path.normalise();
+    normalisen() {
+            this.directories = this.directories.filter(Boolean);
+        }
 
-                const encoder = new PercentEncoder([separator]);
-                this[pEncode] = encoder.encode.bind(encoder);
-
-                return path;
-            },
-            "normalise": function(path, separator = Path.defaultSeparator) {
-                return path.replace(new RegExp(`(?:${separator})+`, "g"), separator);
-            }
-        }));
-
-Object.assign(Path.prototype, {
-    "normalise": function() {
-        this.directories = this.directories.filter(Boolean);
-    },
-    "toString": function() {
+    toString() {
         var parts = this.directories.slice();
         if(this.file) {
             parts.push(this.file);
@@ -43,4 +20,33 @@ Object.assign(Path.prototype, {
 
         return parts.map(this[pEncode]).join(this.separator);
     }
+}
+
+Object.assign(Path, {
+    defaultSeparator: "/",
+    parse(path, separator = Path.defaultSeparator) {
+        const directories = path.split(separator).map(decode);
+
+        var file;
+        if(!path.endsWith(separator)) {
+            file = directories.pop();
+        }
+
+        const path = new Path({
+            "directories": directories,
+            "file": file,
+            "separator": separator
+        });
+        path.normalise();
+
+        const encoder = new PercentEncoder([separator]);
+        this[pEncode] = encoder.encode.bind(encoder);
+
+        return path;
+    },
+    normalise(path, separator = Path.defaultSeparator) {
+        return path.replace(new RegExp(`(?:${separator})+`, "g"), separator);
+    }
 });
+
+export default Path;
