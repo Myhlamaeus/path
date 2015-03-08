5c867de6e140882c19bc18b51965c70067c99cea
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-08 14:48:21 +0000

    Add build scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,8 +35,54 @@ module.exports = function (grunt) {
                 files: "<%= jshint.lib.src %>",
                 tasks: ["jshint:lib", "nodeunit"]
             }
+        },
+        babel: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    "dist/cjs.js": "<%= config.main %>.js"
+                }
+            }
+        },
+        browserify: {
+            dist: {
+                options: {
+                    browserifyOptions: {
+                        standalone: "<%= config.global %>"
+                    }
+                },
+                files: {
+                    "dist/browser.js": "dist/cjs.js"
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    screwIE8: true
+                },
+                files: {
+                    "dist/<%= config.main %>.min.js": "<%= config.main %>.js"
+                }
+            },
+            distCjs: {
+                files: {
+                    "dist/cjs.min.js": "dist/cjs.js"
+                }
+            },
+            distBrowser: {
+                files: {
+                    "dist/browser.min.js": "dist/browser.js"
+                }
+            }
         }
     });
 
     grunt.registerTask("test", ["jshint"]);
+
+    grunt.task.registerTask("build:es6", ["uglify:dist"]);
+    grunt.task.registerTask("build:cjs", ["babel:dist"]);
+    grunt.task.registerTask("build:browser", ["babel:dist", "browserify:dist", "uglify:distBrowser"]);
 };
